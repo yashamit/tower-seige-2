bad49bab89d21e8e7a272ca1372cc49894b78dc3
yashamit <76246828+yashamit@users.noreply.github.com>
2021-04-03 16:07:38 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,35 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.visibility= 255
+      this.image=loadImage("block.png")
+    }
+    display(){
+      var pos= this.body.position;
+      
+       
+      console.log(this.body.speed)
+      if(this.body.speed<3){
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width, this.height);
+       }
+       else{
+        
+         World.remove(world,this.body) 
+         
+         push()
+         this.visibility-=5;
+         tint(255,this.visibility)
+         image(this.image , this.body.position.x , this.body.position.y , 30 , 40)
+         
+         pop()
+       }
+    }
+}
